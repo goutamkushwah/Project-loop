44aa68c0f396534bf3b852e48e108763bebaceed
goutam <goutam.kushwah2003@gmail.com>
2026-07-28 13:41:42 +0530

    Complete Prisma database setup

--- loop/prisma/seed.ts
+++ loop/prisma/seed.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
+
   const workspace = await prisma.workspace.create({
     data: {
       name: "Demo Workspace",
@@ -10,7 +11,46 @@ async function main() {
     },
   });
 
-  console.log(workspace);
+
+  const admin = await prisma.user.create({
+    data: {
+      name: "Admin User",
+      email: "admin@loop.com",
+      password: "password123",
+      role: Role.ADMIN,
+      workspaceId: workspace.id,
+    },
+  });
+
+
+  const analyst = await prisma.user.create({
+    data: {
+      name: "Analyst User",
+      email: "analyst@loop.com",
+      password: "password123",
+      role: Role.ANALYST,
+      workspaceId: workspace.id,
+    },
+  });
+
+
+  const viewer = await prisma.user.create({
+    data: {
+      name: "Viewer User",
+      email: "viewer@loop.com",
+      password: "password123",
+      role: Role.VIEWER,
+      workspaceId: workspace.id,
+    },
+  });
+
+
+  console.log({
+    workspace,
+    admin,
+    analyst,
+    viewer,
+  });
 }
 
 main()
