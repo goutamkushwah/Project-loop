0cf3972ca5e6bd73229ea0d52181e271e297e296
goutam <goutam.kushwah2003@gmail.com>
2026-07-30 16:30:56 +0530

    navbar

--- loop/app/(app)/forbidden/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ForbiddenPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">403 - Forbidden</h1>
-    </div>
-  );
-}
